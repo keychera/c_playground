64c4d20a12f62523c3ec7df49f27ca24c81e0383
keychera <kyogatama@gmail.com>
2024-02-25 01:11:29 +0700

    for windows

---
+++ for_windows.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main()
+{
+    OutputDebugStringA("Hello Windows!");
+    puts("hello windows, the above does not appear sadly https://github.com/microsoft/vscode-cpptools/issues/140 https://github.com/microsoft/vscode-cpptools/issues/3276");
+    int decks;
+    scanf("%i", &decks);
+    
+    return 0;
+}
