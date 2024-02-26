2cfd669215716322ded228e3c99cc1963dc629cc
keychera <kyogatama@gmail.com>
2024-02-26 17:25:02 +0700

    winmain

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 learning from
 - https://www.youtube.com/playlist?list=PLEMXAbCVnmY6RverunClc_DMLNDd3ASRp
 - Head First C
+
+docs visited
+- https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
--- for_windows.c
+++ for_windows.c
@@ -1,12 +1,10 @@
 #include <windows.h>
 #include <stdio.h>
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
     OutputDebugStringA("Hello Windows!");
     puts("hello windows, the above does not appear sadly https://github.com/microsoft/vscode-cpptools/issues/140 https://github.com/microsoft/vscode-cpptools/issues/3276");
-    int decks;
-    scanf("%i", &decks);
     
     return 0;
 }
