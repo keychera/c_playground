cda8956041f62f4e7fe5d065b24eb82fdb25677f
keychera <kyogatama@gmail.com>
2024-07-25 15:52:12 +0900

    loops

--- cards.c
+++ cards.c
@@ -4,26 +4,40 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch (card_name[0])
+    int count = 0;
+    while (card_name[0] != 'X')
     {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case  'A':
-        val = 11;
-        break;
-    default:
-        val = atoi(card_name);
-        break;
+        puts("Enter the card name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+
+        switch (card_name[0])
+        {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            puts("Exiting the program!");
+            continue;
+        default:
+            val = atoi(card_name);
+            if (val < 1 || val > 10)
+            {
+                puts("ERR: Not a valid card!");
+                continue;
+            }
+            break;
+        }
+        if (val >= 3 && val <= 6)
+            count++;
+        else if (val == 10)
+            count--;
+        printf("Current count: %i\n", count);
     }
-    if (val >= 3 && val <= 6)
-        puts("Count has gone up");
-    else if (val == 10)
-        puts("Count has gone down");
     return 0;
 }
